264bb2ea976ae5089817c5a0c725875d990684ef
Shreyans jain <shreyansj2000@gmail.com>
2025-10-18 22:35:50 +0530

    made other es6 modules to CDN

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/0.180.0/three.module.js'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.180.0/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.180.0/examples/jsm/controls/OrbitControls.js';
 // import { GUI } from 'lil-gui'
-
-
 
 const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
